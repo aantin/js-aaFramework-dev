3734defa1f7471ca31d98a90420d67b8d639a95d
Antoine <38399674+aantin@users.noreply.github.com>
2023-10-28 23:14:51 +0900

    fix: center number in pastilles

--- aaFramework.js
+++ aaFramework.js
@@ -21,7 +21,7 @@
     // Public:
     aa.versioning.test({
         name: ENV.MODULE_NAME,
-        version: "3.14.1",
+        version: "3.14.2",
         dependencies: {
             aaJS: "^3.1"
         }

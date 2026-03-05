f867acee1f62a2fdfb8d2f07c0846ae8beb959a6
Antoine <38399674+aantin@users.noreply.github.com>
2026-03-05 12:51:32 +0900

    chore: update version

--- aaFramework.js
+++ aaFramework.js
@@ -24,7 +24,7 @@
     // Public:
     aa.versioning.test({
         name: ENV.MODULE_NAME,
-        version: "3.27.0",
+        version: "3.27.1",
         dependencies: {
             aaJS: "^3.1"
         }
